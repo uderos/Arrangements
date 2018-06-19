f187bb53559a6d6de48d1d7eb9b0612fe35184c4
uderos <uderos@ilww.com>
2018-06-19 09:04:35 -0400

    Dinner shuffles seem to work

--- ece_201806219/Arrangements.cpp
+++ ece_201806219/Arrangements.cpp
@@ -31,7 +31,15 @@ std::vector<std::string> Arrangements::panel_shuffles(int n)
 
 std::vector<std::string> Arrangements::dinner_shuffles(int n)
 {
+	// TODO: be sure n < m_names.size()
+	const std::string names = m_names.substr(0, n);
+	const int guest_idx = 0;
+	std::string shuffle(n, EMPTY_SEAT);
 	std::vector<std::string> shuffle_list;
+
+	generate_dinner_shuffles(names, guest_idx, shuffle, shuffle_list);
+
+	return shuffle_list;
 	return shuffle_list;
 }
 
@@ -99,5 +107,71 @@ void Arrangements::generate_panel_shuffles(
 		shuffle[guest_idx + 1] = EMPTY_SEAT;
 	}
 
+}
+
+int Arrangements::calculate_left_index(const int idx, const int n) const
+{
+	return (idx == 0 ? n - 1 : idx - 1);
+}
+
+int Arrangements::calculate_right_index(const int idx, const int n) const
+{
+	return (idx == n - 1 ? 0 : idx + 1);
+}
+
+
+void Arrangements::generate_dinner_shuffles(
+	const std::string & names,
+	const int guest_idx,
+	std::string & shuffle,
+	std::vector<std::string> & shuffle_list)
+{
+	if (guest_idx == names.size())
+	{
+		std::cout << __FUNCTION__
+			<< " i=" << guest_idx
+			<< " s=" << shuffle
+			<< " ADDING " << shuffle << std::endl;
+		shuffle_list.push_back(shuffle);
+		return;
+	}
+
+	// The guest does not move
+	if (shuffle[guest_idx] == EMPTY_SEAT)
+	{
+		std::cout << __FUNCTION__
+			<< " i=" << guest_idx
+			<< " s=" << shuffle
+			<< ' ' << names[guest_idx] << " stays " << std::endl;
+		shuffle[guest_idx] = names[guest_idx];
+		generate_dinner_shuffles(names, 1 + guest_idx, shuffle, shuffle_list);
+		shuffle[guest_idx] = EMPTY_SEAT;
+	}
+
+	// The guest moves left
+	const int left_idx = calculate_left_index(guest_idx, names.size());
+	if (shuffle[left_idx] == EMPTY_SEAT)
+	{
+		std::cout << __FUNCTION__
+			<< " i=" << guest_idx
+			<< " s=" << shuffle
+			<< ' ' << names[guest_idx] << " moves left to " << left_idx << std::endl;
+		shuffle[left_idx] = names[guest_idx];
+		generate_dinner_shuffles(names, 1 + guest_idx, shuffle, shuffle_list);
+		shuffle[left_idx] = EMPTY_SEAT;
+	}
+
+	// The guest moves right
+	const int right_idx = calculate_right_index(guest_idx, names.size());
+	if (shuffle[right_idx] == EMPTY_SEAT)
+	{
+		std::cout << __FUNCTION__
+			<< " i=" << guest_idx
+			<< " s=" << shuffle
+			<< ' ' << names[guest_idx] << " moves right to " << right_idx << std::endl;
+		shuffle[right_idx] = names[guest_idx];
+		generate_dinner_shuffles(names, 1 + guest_idx, shuffle, shuffle_list);
+		shuffle[right_idx] = EMPTY_SEAT;
+	}
 }
 
--- ece_201806219/Arrangements.h
+++ ece_201806219/Arrangements.h
@@ -23,5 +23,13 @@ private:
 		std::string & shuffle,
 		std::vector<std::string> & shuffle_list);
 
+	void generate_dinner_shuffles(
+		const std::string & names,
+		const int guest_idx,
+		std::string & shuffle,
+		std::vector<std::string> & shuffle_list);
+
+	int calculate_left_index(const int idx, const int n) const;
+	int calculate_right_index(const int idx, const int n) const;
 };
 
